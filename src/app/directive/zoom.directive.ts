d68f5d408603aa5e304f7f25dd8eee3314c88967
Stein <smeetsstein@gmail.com>
2017-09-27 15:09:09 +0200

    improved zoom for max zoom 2

--- src/app/directive/zoom.directive.ts
+++ src/app/directive/zoom.directive.ts
@@ -1,90 +1,104 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2, ViewChild} from "@angular/core";
+import {Directive, ElementRef, HostListener, Input, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Directive({
   selector: '[ng-zoom]'
 })
-export class ZoomDirective {
+export class ZoomDirective implements OnInit {
 
-  private zoomStep:number= 0.1;
-  private pinchStep:number = 0.05;
-  private currentZoom:number= 1;
-  private zoomTarget:Element;
-  private zoomPoint:Point;
+  private zoomStep: number;
+  private pinchStep: number;
+  private currentZoom: number;
+  private zoomTarget: Element;
+  private zoomPoint: Point; // Position of the mouse on the start of the wheel event.
+  private previousZoom: number;
 
-  @Input("minZoom")minZoom:number =1;
-  @Input("maxZoom")maxZoom:number = 2;
+  private zoomPointLocked: boolean;
+  private zoomPointLockTimeout: any;
+  private currentDeviation: Point;
 
-  @HostListener('wheel', ['$event']) onWheel(event){
-    console.log("Wheel", event)
-    if(event.ctrlKey){
+
+  @Input('minZoom')minZoom: number;
+  @Input('maxZoom')maxZoom: number;
+
+  @HostListener('wheel', ['$event']) onWheel(event) {
+    if (event.ctrlKey) {
       event.preventDefault();
-      let ratios = this.getContainerRatios();
-      this.zoomPoint = {x:event.x, y: event.y}
-      this.zoomIntoContainer(this.zoomPoint,ratios, (event.deltaY<0)?this.zoomStep:-this.zoomStep);
+      const ratios = this.getContainerRatios();
+      this.zoomPoint = {x: event.x, y: event.y} ;
+      this.zoomIntoContainer(this.zoomPoint, ratios, (event.deltaY < 0 ) ? this.zoomStep : -this.zoomStep);
     }
   }
-  @HostListener('tap', ['$event']) onTouch(event){
-    console.log("TOUCH", event);
+  @HostListener('tap', ['$event']) onTouch(event) {
+    event.preventDefault();
+    const ratios = this.getContainerRatios();
+    this.zoomPoint = {x: event.center.x, y: event.center.y} ;
+    this.zoomIntoContainer(this.zoomPoint, ratios, this.zoomStep);
   }
-  @HostListener('pinchin', ['$event']) onPinchIn( event){
-    console.log("PINCH", event)
-    if(event.velocityY ==0)return;
-    let ratios = this.getContainerRatios();
-    this.zoomPoint = {x:event.center.x, y: event.center.y}
+  @HostListener('pinchin', ['$event']) onPinchIn( event) {
+    if (event.velocityY === 0) { return; }
+    const ratios = this.getContainerRatios();
+    this.zoomPoint = {x: event.center.x, y: event.center.y};
     this.zoomIntoContainer(this.zoomPoint, ratios, -this.pinchStep);
   }
-  @HostListener('pinchout', ['$event']) onPinchOut( event){
-    console.log("PINCH", event)
-    if(event.velocityY ==0)return;
-    let ratios = this.getContainerRatios();
-    this.zoomPoint = {x:event.center.x, y: event.center.y}
+  @HostListener('pinchout', ['$event']) onPinchOut( event) {
+    if (event.velocityY === 0) { return; }
+    const ratios = this.getContainerRatios();
+    this.zoomPoint = {x: event.center.x, y: event.center.y};
     this.zoomIntoContainer(this.zoomPoint, ratios, this.pinchStep);
   }
 
-  constructor(private render:Renderer2, private elRef:ElementRef){
-
+  constructor(private render: Renderer2, private elRef: ElementRef) {
+    this.zoomStep = 0.1;
+    this.pinchStep = 0.05;
+    this.currentZoom = 1;
+    this.previousZoom = 0;
+    this.minZoom = 1;
+    this.maxZoom = 2;
   }
 
-  private getContainerRatios():any{
+  private getContainerRatios(): any {
     let ratioX, ratioY;
-    if(this.elRef.nativeElement.scrollWidth > this.elRef.nativeElement.scrollHeight){
+    if (this.elRef.nativeElement.scrollWidth > this.elRef.nativeElement.scrollHeight) {
       ratioX = 1;
-      ratioY = this.elRef.nativeElement.scrollHeight/this.elRef.nativeElement.scrollWidth;
-    }else{
-      ratioX = this.elRef.nativeElement.scrollWidth/this.elRef.nativeElement.scrollHeight;
+      ratioY = this.elRef.nativeElement.scrollHeight / this.elRef.nativeElement.scrollWidth;
+    }else {
+      ratioX = this.elRef.nativeElement.scrollWidth / this.elRef.nativeElement.scrollHeight;
       ratioY = 1;
     }
-    return {x:ratioX, y:ratioY};
+    return {x: ratioX, y: ratioY};
   }
-  private zoomIntoContainer(zoomPoint:Point, ratios:any, zoomStep:number){
-    let prevDif = {right: this.zoomTarget.getBoundingClientRect().right - this.zoomPoint.x , bottom:this.zoomTarget.getBoundingClientRect().bottom - this.zoomPoint.y }
+  private zoomIntoContainer(zoomPoint: Point, ratios: any, zoomStep: number) {
+    const prevDif = {
+      right: this.zoomTarget.getBoundingClientRect().right - this.zoomPoint.x ,
+      bottom: this.zoomTarget.getBoundingClientRect().bottom - this.zoomPoint.y
+    };
 
-    if(!this.setNewZoomLevel(zoomStep))return;
+    if (!this.setNewZoomLevel(zoomStep)) { return; }
 
-    this.render.setStyle(this.zoomTarget, "transform", "scale("+this.currentZoom+")");
-    let afterDif = {right: this.zoomTarget.getBoundingClientRect().right - this.zoomPoint.x , bottom:this.zoomTarget.getBoundingClientRect().bottom - this.zoomPoint.y }
+    this.render.setStyle(this.zoomTarget, 'transform', 'scale(' + this.currentZoom + ')');
+    const afterDif = {
+      right: this.zoomTarget.getBoundingClientRect().right - this.zoomPoint.x ,
+      bottom: this.zoomTarget.getBoundingClientRect().bottom - this.zoomPoint.y
+    };
 
-    let scrollLeft = ((afterDif.right-prevDif.right)/2)*ratios.x;
-    let scrollTop = ((afterDif.bottom-prevDif.bottom)/2)*ratios.y;
+    // Scroll to center
+    const scrollLeft = ((afterDif.right - prevDif.right) / this.maxZoom ) // * ratios.x;
+    const scrollTop = ((afterDif.bottom - prevDif.bottom) / this.maxZoom ) // * ratios.y;
 
-   // console.log(this.getTargetCenter())
-    console.log(zoomPoint)
-    //console.log(this.getCenterDeviation(this.getTargetCenter(), zoomPoint));
-
-
-    this.elRef.nativeElement.scrollLeft += scrollLeft + this.getCenterDeviation(this.getTargetCenter(), zoomPoint).x;
-    this.elRef.nativeElement.scrollTop += scrollTop + this.getCenterDeviation(this.getTargetCenter(), zoomPoint).y;
+    this.elRef.nativeElement.scrollLeft += scrollLeft + this.getCenterDeviation(this.getTargetCenter(),
+        zoomPoint, this.getContainerRatios(), zoomStep).x;
+    this.elRef.nativeElement.scrollTop += scrollTop + this.getCenterDeviation(this.getTargetCenter(),
+        zoomPoint, this.getContainerRatios(), zoomStep).y;
   }
 
-  private previousZoom:number = 0;
-  private setNewZoomLevel(zoomStep:number):boolean{
+  private setNewZoomLevel(zoomStep: number): boolean {
     this.currentZoom += zoomStep;
-    if(this.currentZoom < this.minZoom){
+    if (this.currentZoom < this.minZoom) {
       this.currentZoom = this.minZoom;
-      if(this.previousZoom == this.currentZoom)return false;
-    }else if(this.currentZoom > this.maxZoom){
+      if (this.previousZoom === this.currentZoom) { return false; }
+    }else if (this.currentZoom > this.maxZoom) {
       this.currentZoom = this.maxZoom;
-      if(this.previousZoom == this.currentZoom)return false;
+      if (this.previousZoom === this.currentZoom) { return false; }
 
     }
 
@@ -92,25 +106,32 @@ export class ZoomDirective {
 
     return true;
   }
-  private getTargetCenter():Point{
-    let rectangle = this.elRef.nativeElement.getBoundingClientRect();
-    return {x: rectangle.left + (rectangle.width/2) ,y: rectangle.top + (rectangle.height/2)}
+  private getTargetCenter(): Point {
+    const rectangle = this.elRef.nativeElement.getBoundingClientRect();
+    return {x: rectangle.left + (rectangle.width / 2) , y: rectangle.top + (rectangle.height / 2) };
   }
-  private getCenterDeviation(center:Point, zoomPoint:Point):Point{
+  private getCenterDeviation(center: Point, zoomPoint: Point, ratio: Point, zoomStep: number): Point {
+    if (this.zoomPointLockTimeout) {
+      clearTimeout(this.zoomPointLockTimeout);
+    }
+    this.zoomPointLockTimeout = setTimeout(() => {
+      this.zoomPointLocked = false;
+    }, 1000);
+    if (!this.zoomPointLocked) {
+      this.zoomPointLocked = true;
+      const stepsToMaxZoom = (this.maxZoom - this.currentZoom) / zoomStep;
+      this.currentDeviation = {x: (zoomPoint.x - center.x) / stepsToMaxZoom , y: (zoomPoint.y - center.y) / stepsToMaxZoom };
+    }
+    return this.currentDeviation;
 
-      return {x: (zoomPoint.x - center.x)*0.1, y: (zoomPoint.y - center.y)*0.1};
   }
 
-  ngOnInit(){
-    this.zoomTarget = document.getElementById("zoomTarget");
-
-    this.render.listen('document', 'click', (event)=>{
-      console.log("fdfqsd")
-    })
+  ngOnInit() {
+    this.zoomTarget = document.getElementById('zoomTarget');
   }
 }
 
-export class Point{
-  x:number;
-  y:number;
+export class Point {
+  x: number;
+  y: number;
 }
